d189c436d997d75fc45b15b55f617de8e62da322
pdyang <impdyang@163.com>
2017-12-25 11:40:58 +0800

    add file: emacs/init.el

---
+++ emacs/init.el
@@ -0,0 +1,31 @@
+;; Toggle fullscreen
+(toggle-frame-fullscreen)
+;;
+;;
+;;
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(custom-enabled-themes (quote (dichromacy)))
+ '(line-number-mode t)
+ '(column-number-mode t)
+ '(global-linum-mode t)
+ '(size-indication-mode t)
+ '(menu-bar-mode nil)
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil)
+ '(next-line-add-newlines t))
+;;
+;;
+;;
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Noto Mono" :foundry "monotype" :slant normal :weight normal :height 128 :width normal)))))
+(put 'erase-buffer 'disabled nil)
