5db954bc45485f1c6156f70a7e43e3dcb38e0565
pdyang <impdyang@163.com>
2018-01-04 19:23:26 +0800

    将配置文件模块化

--- emacs/init.el
+++ emacs/init.el
@@ -1,27 +1,30 @@
-;; Toggle fullscreen
-(toggle-frame-fullscreen)
-;;
-;;
-;;
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+
+(require 'init-ui)
+(require 'init-better-default)
+
+;; 开启全局补全
+(global-company-mode)
+
+;; 变量设置
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(custom-enabled-themes (quote (dichromacy)))
- '(line-number-mode t)
- '(column-number-mode t)
- '(global-linum-mode t)
- '(size-indication-mode t)
- '(menu-bar-mode nil)
- '(scroll-bar-mode nil)
- '(tool-bar-mode nil)
- '(next-line-add-newlines t))
-;;
-;;
-;;
+ '(custom-enabled-themes (quote (monokai)))
+ '(custom-safe-themes
+   (quote
+    ("8c4acde8417375abc15f41f56d1ddad62e13a4bd886d381ea1a1d4620c932933" default)))
+  '(package-selected-packages (quote (company))))
+
+;; 界面设置
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -29,3 +32,9 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Noto Mono" :foundry "monotype" :slant normal :weight normal :height 128 :width normal)))))
 (put 'erase-buffer 'disabled nil)
+
+;; 将配置文件绑定到F2上
+(defun open-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+(global-set-key (kbd "<f2>") 'open-init-file)
---
+++ emacs/lisp/init-better-default.el
@@ -0,0 +1,47 @@
+;; Hightlight current line
+(global-hl-line-mode t)
+
+;; Change auto-backup directory
+(setq make-backup-files nil)
+
+;;
+(delete-selection-mode t)
+
+;;
+(global-linum-mode t)
+
+;;
+(setq next-line-add-newlines t)
+
+;;
+(setq line-number-mode nil)
+(setq column-number-mode t)
+
+;;
+(display-time-mode t)
+
+;;
+(size-indication-mode t)
+
+(provide 'init-better-default)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
