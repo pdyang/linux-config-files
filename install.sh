8bc78f43c81fbcc57d974e26a9c327a30004e86f
pdyang <impdyang@163.com>
2018-02-16 13:39:09 +0800

    change install.sh and vimrc

--- install.sh
+++ install.sh
@@ -1,26 +1,90 @@
 #!/bin/bash
 
-dirname=$(dirname $0)
+DIRNAME=$(dirname $0)
 
-function printMenu() {
-    echo -e "\t\tInstall Configuration Files"
+function Usage()
+{
+    cat <<-EOF
+		Install the config files of myself.
+
+		Usage: `basename $0` [选项]
+		选项 :
+		       --bash      Install bash config files.
+		       --emacs     Install emacs config files.
+		       --vim       Install vim config files.
+               --git       Config git.
+               --dev       Install development tools.
+		       --a         Install all of them.
+		       --help      Show this help.
+	EOF
 }
 
-function installConfigFiles() {
-    for file in `ls | grep ^bash`
+function InstallBashConfigFiles()
+{
+    for FILE in `ls | grep ^bash`
     do
-        cp -v $file ~/".$file"
+	cp -v $FILE ~/".$FILE"
     done
-
-    if [ -f $dirname/vim/vimrc ]; then
-        cp -v $dirname/vim/vimrc ~/.vimrc
+    echo
+}
+function InstallVimConfigFiles()
+{
+    if [ -f $DIRNAME/vim/vimrc ]; then
+	    cp -v $DIRNAME/vim/vimrc ~/.vimrc
+    else
+	    exit
     fi
-
-    if [ -d $dirname/emacs ]; then
-	cp -vr $dirname/emacs/* ~/.emacs.d
+    echo
+}
+function InstallEmacsConfigFiles()
+{
+    if [ -d $DIRNAME/emacs ]; then
+	    cp -vr $DIRNAME/emacs/* ~/.emacs.d
     fi
+    echo
+}
+function InstallDevTools()
+{
+    sudo apt update && sudo apt install git qt5-default \
+        build-essential evince typora
+}
+function ConfigGit()
+{
+    git config --global user.name pdyang
+    git config --global user.email impdyang@163.com
 }
 
-printMenu
-
-installConfigFiles
+case "$1" in
+	--[Bb][Aa][Ss][Hh]		)
+		InstallBashConfigFiles
+		exit 0
+        ;;
+	--[Ee][Mm][Aa][Cc][Ss]	)
+		InstallEmacsConfigFiles
+		exit 0
+        ;;
+	--[Vv][Ii][Mm]			)
+		InstallVimConfigFiles
+		exit 0
+        ;;
+    --[Dd][Ee][Vv]          )
+        InstallDevTools
+        exit 0
+        ;;
+    --[Gg][Ii][Tt]          )
+        ConfigGit
+        exit 0
+        ;;
+	-a | ""                 )
+		InstallBashConfigFiles
+		InstallEmacsConfigFiles
+		InstallVimConfigFiles
+        InstallDevTools
+        ConfigGit
+		exit 0
+        ;;
+	*						)
+		Usage
+		exit 0
+        ;;
+esac
